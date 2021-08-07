b7a551d8003c3e17a15af45f112115ca4073863b
Grees-Sudo <greesgh06@gmail.com>
2021-08-07 11:40:47 +0530

    Fixed Shit

--- Spammer.py
+++ Spammer.py
@@ -1,7 +1,7 @@
 import pyautogui, time
 time.sleep(5)
-f=open("ThingToSpam.txt", 'r')
-for word in f:
+i=open("ThingToSpam.txt", 'r')
+for word in i:
   pyautogui.typewrite(word)
   pyautogui.press("enter")
     
